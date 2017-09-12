5ed1a2722f8b33621409cde5be3a8ac10859166a
Sony Mathew <ynos1234@gmail.com>
2017-09-13 00:06:37 +0530

    change sin teh subset question -optimisation

--- questions/24_kth_subset.rb
+++ questions/24_kth_subset.rb
@@ -42,38 +42,62 @@ class Array
   end
 end
 
-def get_kth_subset(arr, k)
+# def get_kth_subset(arr, k)
   
-  # genrate all subsets of a given set in order
-  # total 2 ^ n subsets are there
-  # total = 1 << arr.size
+#   # genrate all subsets of a given set in order
+#   # total 2 ^ n subsets are there
+#   # total = 1 << arr.size
 
-  # 0.step(total - 1, 1).each do |i|
-  #   sum = 0
-  #   new_arr = []
+#   # 0.step(total - 1, 1).each do |i|
+#   #   sum = 0
+#   #   new_arr = []
 
-  #   0.step(i - 1, 1).each do |j|
-  #     if (i & (1 << j)) > 0
-  #       sum += arr[j]
-  #       new_arr << arr[j]
-  #     end
-  #   end
-  # end
+#   #   0.step(i - 1, 1).each do |j|
+#   #     if (i & (1 << j)) > 0
+#   #       sum += arr[j]
+#   #       new_arr << arr[j]
+#   #     end
+#   #   end
+#   # end
 
-  new_arr = []
-  # get only kth subset
-  0.step(k - 2, 1).each do |j|
-    new_arr << arr[j] if ((k - 1) & (1 << j)) > 0
+#   new_arr = []
+#   # get only kth subset
+#   0.step(k - 2, 1).each do |j|
+#     new_arr << arr[j] if ((k - 1) & (1 << j)) > 0
+#   end
+#   new_arr
+# end
+
+def kth_subset(old_subsets, current_el, k)
+  temp = []
+  current_list = []
+  current_list.push(current_el)
+
+  old_subsets.each do |s|
+    temp.push(s + current_list)
   end
-  new_arr
+
+  new_subset = old_subsets + temp
+
+  new_subset.sort_by { |s| s.reduce(0, :+) }
+
+  req_range = (k > new_subset.size - 1) ? k : -1
+  new_subset[0..req_range]
 end
 
 def init
+  k = 600000
   arr = gets.chomp()
   arr = arr.split(',').map(&:strip).map(&:to_i)
   arr = arr.counting_sort
-  get_kth_subset(arr, 4)
+  # get_kth_subset(arr, 600000)
+
+  req_subsets = [[]]
+  arr.size.times.each do |i|
+    req_subsets = kth_subset(req_subsets, arr[i], k)
+  end
+  req_subsets[k - 1]
 end
 
 init
-1,2,3
+1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20
